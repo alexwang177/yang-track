1e1d8795da2355adb4793ca760e33f3a0e6b3837
Alex Wang <jianuo.wang@duke.edu>
2019-10-25 12:32:06 -0400

    form style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Title from './components/Title';
+import WordForm from './components/WordForm';
 
 function App() {
   return (
     <div className="App">
       <Title></Title>
-      
+      <WordForm></WordForm>
     </div>
   );
 }
